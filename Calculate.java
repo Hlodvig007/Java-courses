4c463ac27a4552c4c17c5ce2b4c2e733cc91666f
Sasha <Hlodvig007@gmail.com>
2015-12-30 22:19:44 +0200

    4 calculate operations

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,15 @@
 public class Calculate {
     public static void main (String[] arg){
-        System.out.println ("Hello world");
+        System.out.println ("Calculate");
+        int first = Integer.valueOf (arg[0]);
+        int second = Integer.valueOf (arg[1]);
+        int summ = first + second;
+        int multiply = first * second;
+        int division = first / second;
+        int subtraction = first - second;
+        System.out.println ("Summ " + summ);
+        System.out.println ("Multiply " + multiply);
+        System.out.println ("Division " + division);
+        System.out.println ("Subtraction " + subtraction);
     }
 }
